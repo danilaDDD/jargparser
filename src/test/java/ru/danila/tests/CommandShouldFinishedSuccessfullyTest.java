d07da9122559d26bb0cf3159c6389666df28f88a
danila nikitin <danila.n2015@yandex.ru>
2024-06-17 15:55:04 +0300

    rename to CommandShouldFinishedSuccessfullyTest, rename it methods and add CommandMixedManyDifferentKeyAndDifferentPositionArgument

--- src/test/java/ru/danila/tests/CommandShouldFinishedSuccessfullyTest.java
+++ src/test/java/ru/danila/tests/CommandShouldFinishedSuccessfullyTest.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
-public class SuccessParseByArgParserTests {
+public class CommandShouldFinishedSuccessfullyTest {
     private final ArgParser argParser = ArgParser.of();
 
     @ParameterizedTest
@@ -50,18 +50,6 @@ public class SuccessParseByArgParserTests {
         assertTrue(success);
     }
 
-    private static <T> boolean equalsPositionValues(HandleArgs handleArgs, List<T> expectedValues){
-        boolean equals = true;
-        for (int i = 0; i < expectedValues.size(); i++) {
-            if(!expectedValues.get(i).equals(handleArgs.<String>getPositionValue(0))){
-                equals = false;
-                break;
-            }
-        }
-
-        return equals;
-    }
-
     private static class TestableCommandHandler implements CommandHandler{
         private CommandHandler commandHandler;
 
@@ -85,12 +73,13 @@ public class SuccessParseByArgParserTests {
 
     private static Stream<Arguments> getArgs(){
         return Stream.of(
-                getRepeatedStringTestArguments(),
-                getIntegerRepeatTestArguments()
+                commandWith1KeyParamAna2PositionParamArgument(),
+                CommandWith1KeyRepeatedAnd2PositionStringArgument(),
+                CommandMixedManyDifferentKeyAndDifferentPositionArgument()
         );
     }
 
-    private static Arguments getRepeatedStringTestArguments(){
+    private static Arguments commandWith1KeyParamAna2PositionParamArgument(){
         List<String> expectedArgValues = List.of("key1=value1", "key2=value2", "key3=value3");
         KeyCommandParam param = KeyCommandParam.builder()
                 .setShortName("e")
@@ -103,31 +92,95 @@ public class SuccessParseByArgParserTests {
                 .addPositionParam(new PositionCommandParam(ParamType.INTEGER))
                 .addPositionParam(new PositionCommandParam(ParamType.DOUBLE))
                 .setCommandArg("1 1.2 -e key1=value1 --env key2=value2 -e key3=value3")
+
+                .addHandler(new TestableCommandHandler((handleArgs) -> {
+                    assertEquals(1, handleArgs.<Integer>getPositionValue(0));
+                    assertEquals(1.2d, handleArgs.<Double>getPositionValue(1));
+                    assertEquals(expectedArgValues, handleArgs.getKeyValue("e"));
+                }))
+
                 .build();
 
         return Arguments.of(args);
     }
 
-    private static Arguments getIntegerRepeatTestArguments(){
-        String shortName = "i";
+    private static Arguments CommandWith1KeyRepeatedAnd2PositionStringArgument(){
         var param = KeyCommandParam.builder()
                 .setRepeated(true)
                 .setParamType(ParamType.INTEGER)
-                .setShortName(shortName)
+                .setShortName("i")
                 .setFullName("int")
                 .build();
-
-        List<Integer> expectedKeyValues = List.of(1, 2, 3);
-        List<String> expectedPositionValues = List.of("word1", "word2");
 
         ParserArgs args = ParserArgs.builder()
                 .addPositionParam(new PositionCommandParam())
                 .addKeyParam(param)
                 .setCommandArg("--int 1 -i 2 --int 3 word1 word2")
                 .addHandler(new TestableCommandHandler((handleArgs) -> {
-                    assertTrue(equalsPositionValues(handleArgs, expectedPositionValues));
-                    assertEquals(handleArgs.<List<Integer>>getKeyValue("i"), expectedKeyValues);
+                    assertEquals("word1", handleArgs.getPositionValue(0));
+                    assertEquals("word2", handleArgs.getPositionValue(1));
+                    assertEquals(handleArgs.getKeyValue("i"), List.of(1, 2, 3));
                 }))
+                .build();
+
+        return Arguments.of(args);
+    }
+
+    private static Arguments CommandMixedManyDifferentKeyAndDifferentPositionArgument(){
+        KeyCommandParam nmParam, tParam, pParam, iParam;
+        nmParam = KeyCommandParam.builder()
+                .setParamType(ParamType.DOUBLE)
+                .setShortName("nm")
+                .setFullName("number")
+                .setRequired(true)
+                .setRepeated(true)
+                .setDescription("number")
+                .build();
+
+        tParam = KeyCommandParam.builder()
+                .setParamType(ParamType.NON_ARGUMENT)
+                .setShortName("t")
+                .setFullName("t")
+                .setRequired(false)
+                .setRepeated(false)
+                .build();
+
+        pParam = KeyCommandParam.builder()
+                .setShortName("p")
+                .setFullName("ps")
+                .setRepeated(false)
+                .setRequired(false)
+                .build();
+
+        iParam = KeyCommandParam.builder()
+                .setParamType(ParamType.INTEGER)
+                .setShortName("i")
+                .setFullName("integer")
+                .setRepeated(false)
+                .setRequired(true)
+                .build();
+
+        ParserArgs args = ParserArgs.builder()
+                .setCommandArg("-nm 1 --number 2.2 -nm 3 10 -t -p password 2.9 -i 1 --integer 2 three")
+                .addKeyParam(pParam)
+                .addKeyParam(iParam)
+                .addKeyParam(nmParam)
+                .addKeyParam(tParam)
+                .addPositionParam(new PositionCommandParam(ParamType.INTEGER)) // 10
+                .addPositionParam(new PositionCommandParam(ParamType.DOUBLE)) // 2.9
+                .addPositionParam(new PositionCommandParam()) // three
+
+                .addHandler(new TestableCommandHandler((handleArgs) ->{
+                    assertEquals(List.of(1d, 2.2, 3), handleArgs.getKeyValue("nm"));
+                    assertEquals(true, handleArgs.getKeyValue("t"));
+                    assertEquals("password", handleArgs.getKeyValue("p"));
+                    assertEquals(List.of(1, 2), handleArgs.getKeyValue("i"));
+
+                    assertEquals(10, handleArgs.<Integer>getPositionValue(0));
+                    assertEquals(2.9d, handleArgs.getPositionValue(1));
+                    assertEquals("three", handleArgs.getPositionValue(2));
+                }))
+
                 .build();
 
         return Arguments.of(args);
